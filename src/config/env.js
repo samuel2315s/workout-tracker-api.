1a1287c5bbf067f5ee55c5f1292ed055766e75be
samuel2315s <samuvanegasq23@gmail.com>
2026-09-14 15:47:34 -0500

    se configuro las rutas versionadas y se creo las rutas

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 3000,//aca dice que si no hay puerto 8000 utilice el 3000
   db: {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
