1cd0f51e79fb7b790e913eedf3f168a1ac6c5dd8
samuel2315s <samuvanegasq23@gmail.com>
2026-09-17 15:35:15 -0500

    feat:implementación de rutas GET (lista e individual)

--- src/routes/v1/users.routes.js
+++ src/routes/v1/users.routes.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
+let users = [
+  {
+    id: "1",
+    name: "Carlos Navia",
+    email: "carlos@example.com",
+    role: "user",
+    createdAt: "2025-09-12T12:00:00Z"
+  }
+];
+
+// GET /api/v1/users
 router.get('/', (req, res) => {
-  res.send('Get all users');
+  res.status(200).json(users);
+});
+
+// GET /api/v1/users/:id
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const user = users.find(u => u.id === id);
+  if (!user) {
+    return res.status(404).json({ error: 'Usuario no encontrado' });
+  }
+  res.status(200).json(user);
 });
 
 module.exports = router;
