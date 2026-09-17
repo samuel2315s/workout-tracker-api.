de5013f8e62a1cd73da930ba75c5371f0329299b
samuel2315s <samuvanegasq23@gmail.com>
2026-09-17 15:33:52 -0500

    feat: scaffold de router y endpoints básicos

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const usersRoutes = require('./users.routes');
 
-router.get('/', (req, res) => {
-  res.send('Get all users');
-});
+router.use('/users', usersRoutes);
 
 module.exports = router;
