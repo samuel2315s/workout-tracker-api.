f7740c3726056a6800f63fd25020968504c4a2ac
samuel2315s <samuvanegasq23@gmail.com>
2026-09-18 18:17:15 -0500

    feat: implementación de rutas GET (lista e individual)

--- src/routes/v1/workouts.routes.js
+++ src/routes/v1/workouts.routes.js
@@ -7,4 +7,7 @@ const workoutsController = require('../../controllers/workouts.controller');
 // GET /api/v1/workouts
 router.get('/', workoutsController.getAllWorkouts);
 
+// GET /api/v1/workouts/:id
+router.get('/:id', workoutsController.getWorkoutById);
+
 module.exports = router;
